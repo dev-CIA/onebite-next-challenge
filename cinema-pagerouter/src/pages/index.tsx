b0582ddd1279a5ff4012d8f3afc09f8965c0e2bb
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2025-04-07 19:09:22 +0900

    feat: 메인 페이지 UI 구현

--- cinema-pagerouter/src/pages/index.tsx
+++ cinema-pagerouter/src/pages/index.tsx
@@ -1,11 +1,30 @@
 import SearchbarLayout from "@/components/searchbar-layout";
 import { ReactNode } from "react";
+import movies from "@/mock/dummy.json";
+import MovieItem from "@/components/movie-item";
+import { MovieData } from "@/types";
+import style from "./index.module.css";
 
 export default function Home() {
   return (
-    <>
-      <h1>ONEBITE CINEMA</h1>
-    </>
+    <div className={style.container}>
+      <section>
+        <h3>지금 가장 추천하는 영화</h3>
+        <div className={style.recommend}>
+          {movies.slice(0, 3).map((movie: MovieData) => (
+            <MovieItem key={movie.id} {...movie} />
+          ))}
+        </div>
+      </section>
+      <section>
+        <h3>등록된 모든 영화</h3>
+        <div className={style.all}>
+          {movies.map((movie: MovieData) => (
+            <MovieItem key={movie.id} {...movie} />
+          ))}
+        </div>
+      </section>
+    </div>
   );
 }
 
