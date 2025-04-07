67e7d68b7e0ad7fca3072107c5f84ee884fea06f
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2025-04-07 19:11:20 +0900

    feat: 서치 페이지 UI 구현

--- cinema-pagerouter/src/pages/search/index.tsx
+++ cinema-pagerouter/src/pages/search/index.tsx
@@ -1,11 +1,32 @@
+import MovieItem from "@/components/movie-item";
 import SearchbarLayout from "@/components/searchbar-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import style from "./index.module.css";
+import movies from "@/mock/dummy.json";
+import { MovieData } from "@/types";
 
 export default function Page() {
   const router = useRouter();
-  const { q } = router.query;
-  return <h1>검색결과 : {q}</h1>;
+  const q = router.query.q as string;
+
+  const results = movies.filter((movie: MovieData) =>
+    new RegExp(q, "i").test(movie.title)
+  );
+
+  return (
+    <>
+      {results.length !== 0 ? (
+        <div className={style.container}>
+          {results.map((movie) => (
+            <MovieItem key={movie.id} {...movie} />
+          ))}
+        </div>
+      ) : (
+        <div>검색 결과가 없습니다.</div>
+      )}
+    </>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
