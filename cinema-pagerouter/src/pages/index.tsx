3a3874d3f95b813ded3f4aa3dfeff3a0b8ea718f
Inae Choi <inaechoi.dev@gmail.com>
2025-04-08 23:39:48 +0900

    feat: index페이지 ISR 방식으로 변경

--- cinema-pagerouter/src/pages/index.tsx
+++ cinema-pagerouter/src/pages/index.tsx
@@ -4,26 +4,26 @@ import MovieItem from "@/components/movie-item";
 import style from "./index.module.css";
 import { InferGetStaticPropsType } from "next";
 import fetchMovies from "@/lib/fetch-movies";
-// import fetchRandomMovies from "@/lib/fetch-random-movies";
+import fetchRandomMovies from "@/lib/fetch-random-movies";
 
 export const getStaticProps = async () => {
-  // const [allMovies, recoMovies] = await Promise.all([
-  //   fetchMovies(),
-  //   fetchRandomMovies(),
-  // ]);
-  const allMovies = await fetchMovies();
-  return { props: { allMovies } };
+  const [allMovies, recoMovies] = await Promise.all([
+    fetchMovies(),
+    fetchRandomMovies(),
+  ]);
+  return { props: { allMovies, recoMovies }, revalidate: 10 };
 };
 
 export default function Home({
   allMovies,
+  recoMovies,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
         <div className={style.recommend}>
-          {allMovies.slice(0, 3).map((movie) => (
+          {recoMovies.slice(0, 3).map((movie) => (
             <MovieItem key={movie.id} {...movie} />
           ))}
         </div>
