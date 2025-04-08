65442fa00dd4a153b14c64b318569c88af1280ca
Inae Choi <inaechoi.dev@gmail.com>
2025-04-08 18:03:11 +0900

    feat: index페이지를 SSG로 변경

--- cinema-pagerouter/src/pages/index.tsx
+++ cinema-pagerouter/src/pages/index.tsx
@@ -2,28 +2,28 @@ import SearchbarLayout from "@/components/searchbar-layout";
 import { ReactNode } from "react";
 import MovieItem from "@/components/movie-item";
 import style from "./index.module.css";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetStaticPropsType } from "next";
 import fetchMovies from "@/lib/fetch-movies";
-import fetchRandomMovies from "@/lib/fetch-random-movies";
+// import fetchRandomMovies from "@/lib/fetch-random-movies";
 
-export const getServerSideProps = async () => {
-  const [allMovies, recoMovies] = await Promise.all([
-    fetchMovies(),
-    fetchRandomMovies(),
-  ]);
-  return { props: { allMovies, recoMovies } };
+export const getStaticProps = async () => {
+  // const [allMovies, recoMovies] = await Promise.all([
+  //   fetchMovies(),
+  //   fetchRandomMovies(),
+  // ]);
+  const allMovies = await fetchMovies();
+  return { props: { allMovies } };
 };
 
 export default function Home({
   allMovies,
-  recoMovies,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
         <div className={style.recommend}>
-          {recoMovies.map((movie) => (
+          {allMovies.slice(0, 3).map((movie) => (
             <MovieItem key={movie.id} {...movie} />
           ))}
         </div>
