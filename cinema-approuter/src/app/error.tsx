dcaf1a33f9d7cace76bc686abbfac9da589914b2
Inae Choi <inaechoi.dev@gmail.com>
2025-04-16 16:46:50 +0900

    feat: 에러 핸들링

---
+++ cinema-approuter/src/app/error.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { startTransition, useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.error(error.message);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>오류가 발생했습니다.</h2>
+      <p>{error.message}</p>
+      <button
+        onClick={() => {
+          startTransition(() => {
+            router.refresh();
+            reset();
+          });
+        }}
+      >
+        다시 시도
+      </button>
+    </div>
+  );
+}
