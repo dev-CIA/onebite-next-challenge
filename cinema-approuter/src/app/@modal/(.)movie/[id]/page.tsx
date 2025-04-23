609e597fbe50f032204d3a0ddffd17de5c13429a
Inae Choi <inaechoi.dev@gmail.com>
2025-04-24 02:16:08 +0900

    feat: no-explicit-any

--- cinema-approuter/src/app/@modal/(.)movie/[id]/page.tsx
+++ cinema-approuter/src/app/@modal/(.)movie/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Modal from "@/components/modal";
 import MoviePage from "@/app/movie/[id]/page";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function Page(props: any) {
   return (
     <Modal>
