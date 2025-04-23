9ab522d6b3613f2f3026771478217e368c2c5f0b
Inae Choi <inaechoi.dev@gmail.com>
2025-04-24 02:03:12 +0900

    feat: 이미지 최적화

--- cinema-approuter/src/components/movie-item.tsx
+++ cinema-approuter/src/components/movie-item.tsx
@@ -1,11 +1,19 @@
 import { MovieData } from "@/types";
 import Link from "next/link";
 import style from "./movie-item.module.css";
+import Image from "next/image";
 
-export default function MovieItem({ id, posterImgUrl }: MovieData) {
+export default function MovieItem({ id, title, posterImgUrl }: MovieData) {
   return (
     <Link href={`/movie/${id}`} className={style.container}>
-      <img src={posterImgUrl} />
+      <div>
+        <Image
+          src={posterImgUrl}
+          fill
+          sizes="156px"
+          alt={`영화 ${title} 포스터 이미지`}
+        />
+      </div>
     </Link>
   );
 }
